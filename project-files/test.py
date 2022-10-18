e04cea1109f9dd3269f4d4f75b59b4a8dfcab816
tw0116 <tw0116@outlook.com>
2022-10-18 13:36:57 +0800

    Added "test.py" file

---
+++ project-files/test.py
@@ -0,0 +1,18 @@
+# Temp Datasets
+
+morningPeakHours = []
+eveningPeakHours = []
+nonPeakHours = []
+
+
+station_start = str(input("Enter Station: "))
+station_end = str(input("Enter Station: "))
+
+# A function to find the line of the "station_start" and "station_end"
+# Check whether the "station_start" and the "station_end" at the same line.
+
+    # if "station_start" and "station_end" are at the same line
+        # alther heuristic function
+
+    # if "station_start" and "station_end" are not at the same line
+        # alther heuristic function
