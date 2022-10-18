855587867128a769b6422baa003ce6c8342d0932
samidare210 <72958390+samidare210@users.noreply.github.com>
2022-10-18 14:26:34 +0800

    update on data set

--- project-files/mtr_api.py
+++ project-files/mtr_api.py
@@ -2,20 +2,28 @@ from urllib import response
 import requests
 import json
 
-line = str(input("Enter Line: "))
-station = str(input("Enter Station: "))
 
-payload = {"line": line, "sta": station}
 
-r = requests.get(
+
+
+print(len(tml_stat))
+
+#line = str(input("Enter Line: "))
+#station = str(input("Enter Station: "))
+
+for i in tml_stat:
+
+    payload = {"line": "TML", "sta": i}
+
+    r = requests.get(
     "https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?", params=payload)
 
-mtr_obj = json.dumps(r.json(), indent=4)
+    tmt_stat_obj = json.dumps(r.json(), indent=4)
 
-print(mtr_obj)
+    print(tmt_stat_obj)
 
-with open("mtr.json", "w") as file:
-    file.write(mtr_obj)
+    with open("tml_stat.json", "w") as file:
+        file.write(tmt_stat_obj)
 
 
 # file.write(data)
